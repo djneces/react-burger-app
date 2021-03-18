cfcaa4a3fa38d7c6b9edd444e96f16cb646d288f
djneces <j.necesanek@hotmail.com>
2021-03-18 11:14:50 +0100

    Add Redux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,33 +1,10 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-  state = {
-    ingredients: null,
-    totalPrice: 0,
-  };
-
-  componentWillMount() {
-    //extracts search query from the URL passed from the BurgerBuilder
-    const query = new URLSearchParams(this.props.location.search);
-
-    const ingredients = {};
-    let price = 0;
-
-    for (let param of query.entries()) {
-      //each entry => ['salad', '1']
-      if (param[0] === 'price') {
-        price = param[1];
-      } else {
-        ingredients[param[0]] = +param[1]; // => object
-      }
-    }
-
-    this.setState({ ingredients: ingredients, totalPrice: price });
-  }
-
   checkoutCancelledHandler = () => {
     //special props from the Route in App.js
     this.props.history.goBack();
@@ -41,26 +18,28 @@ class Checkout extends Component {
     return (
       <div>
         <CheckoutSummary
-          ingredients={this.state.ingredients}
+          ingredients={this.props.ings}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
         />
         {/* dynamic route  */}
         <Route
           path={this.props.match.path + '/contact-data'}
-          //   I can use component or if I need to pass manually props I use render func
-          //if we do it like that in ContactData we don't have access to props.history => we have to pass props here
-          render={(props) => (
-            <ContactData
-              ingredients={this.state.ingredients}
-              price={this.state.totalPrice}
-              {...props}
-            />
-          )}
+          component={ContactData}
         />
       </div>
     );
   }
 }
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  return {
+    //state.ingredients -> named exactly as in reducer
+    ings: state.ingredients,
+  };
+};
+
+//we don't need dispatch here
+
+//if I don't need mapStateToProps, but I use dispatch => export default connect(null, mapDis)(Checkout);
+export default connect(mapStateToProps)(Checkout);
---
+++ src/store/reducer.js
@@ -0,0 +1,50 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  ingredients: {
+    salad: 1,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  },
+  totalPrice: 4,
+};
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  bacon: 0.7,
+  cheese: 0.4,
+  meat: 1.3,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENTS:
+      //immutability
+      return {
+        ...state,
+        //we need to clone this too, ingredients is another object
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+        //I update ingredients and price in this action, I could separate price in another action too
+        //state.totalPrice ==== the old total price
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+      };
+    case actionTypes.REMOVE_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          //action => payload from BurgerBuilder
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
